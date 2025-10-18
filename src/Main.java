6a492c9778c4807b10dc8abbe68f2a4ceb4a8640
sameperson3108 <sameperson3108@github.com>
2025-10-18 20:31:30 +0700

    коммит

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@
 public class Main {
     public static void main(String[] args) {
         Task tryTask = new Task (01, "помыть посуду", false);
+        TaskList taskList = new TaskList();
+        taskList.addTask("написать список задач");
+        taskList.addTask("вторая задача");
+
         System.out.println(tryTask);
+
+        System.out.print(taskList.getTasks());
     }
 }
--- src/TaskList.java
+++ src/TaskList.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskList {
+    private final List<Task> tasks = new ArrayList<>();
+    private int nextId = 1;
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(String title) {
+        tasks.add(new Task(nextId++, title, false));
+    }
+
+    public Task getById(int id) {
+        return tasks.stream()
+                .filter(task -> task.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean removeTask(int id) {
+        return tasks.removeIf(task -> task.getId() == id);
+    }
+
 }
